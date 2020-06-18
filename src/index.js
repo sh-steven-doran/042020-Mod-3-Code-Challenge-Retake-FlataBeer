61384255ddec8968d01b77f7ccfbcca8a339f191
byoon97 <byoon5397@gmail.com>
2020-06-18 10:10:57 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(e) {
     const beerURL = 'http://localhost:3000/beers'
     const beerUL = document.getElementById('beer-list')
-    const beerCard = document.getElementById('beer-detail')
+    let beerCard = document.getElementById('beer-detail')
 
     fetch(beerURL)
     .then(resp => resp.json())
@@ -17,20 +17,18 @@ document.addEventListener('DOMContentLoaded', function(e) {
     
         document.addEventListener('click', function(e){
             let id = e.target.dataset.id
-            console.log(e.target)
             fetch(beerURL + `/${id}`)
             .then(resp => resp.json())
             .then(beer => {
-                let displayBeer = document.createElement('div')
-                displayBeer = ""
-                displayBeer.innerHTML = `
+                
+                beerCard.innerHTML = ""
+                beerCard.innerHTML = `
                 <h1>${beer.name}</h1>
                 <img src=${beer.image_url}>
                 <h3>${beer.tagline}</h3>
                 <p>${beer.description}</p>
                 `
-
-                beerCard.append(displayBeer)
+                // console.log(beerCard)
             })
         })
     }))
